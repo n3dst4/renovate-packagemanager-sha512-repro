74a7491098ba051f9ba313ea894cb16ff72bfd86
Neil de Carteret <git@lumphammer.net>
2025-07-14 13:01:56 +0100

    remove minirouter project

--- packages/minirouter/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./components/DevTools";
-export * from "./components/Link";
-export * from "./components/Route";
-export * from "./components/Router";
-export * from "./createDirection";
-export type * from "./types";
-export * from "./useNavigationContext";
-export * from "./useParams";
-export * from "./useRoute";
